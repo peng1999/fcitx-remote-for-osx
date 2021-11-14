1d7089da1a1685d663bcc58b6617878288e92102
Peng Guanwen <pg999w@outlook.com>
2021-11-14 12:03:54 +0800

    Python 2 to 3

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-#
+#!/usr/bin/env python3
+
 #  This file is part of fcitx-remote-for-osx
 #  Copyright (c) 2017 fcitx-remote-for-osx's authors
 #  This program is free software: you can redistribute it and/or modify
@@ -45,26 +45,26 @@ def build(imname):
     use_path = BUILD_PATH % imname
     use_opts = OPTS % (InputMethod[imname], use_path)
     use_cmd = '%s %s %s' % (CC, SOURCE_FILE, use_opts)
-    print use_cmd
+    print(use_cmd) 
     if os.system(use_cmd) == 0:
         return imname
     else:
         return False
 
 def build_all():
-    for imname in InputMethod.iterkeys():
+    for imname in InputMethod.keys():
         if build(imname) == False:
             sys.exit(1)
 
 def print_help():
-    print './build.py build all\n./build.py clean'
-    print './build.py build [im]'
-    print 'im:'
-    for im in InputMethod.iterkeys():
-        print '\t %s' % im
+    print('./build.py build all\n./build.py clean')
+    print('./build.py build [im]')
+    print('im:')
+    for im in InputMethod.keys():
+        print('\t %s' % im)
 
 def clean():
-    for imname in InputMethod.iterkeys():
+    for imname in InputMethod.keys():
         use_path = BUILD_PATH % imname
         os.system('rm %s' % use_path)
 
